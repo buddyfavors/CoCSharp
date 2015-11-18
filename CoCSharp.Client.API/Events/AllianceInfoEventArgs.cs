6b89ab7d82b7ca55fbccb3a14296b8495523494c
Buddy Favors Jr <buddyfavors@gmail.com>
2015-11-17 23:45:37 -0500

    Added new EventArg for AllianceInfo.

---
+++ CoCSharp.Client.API/Events/AllianceInfoEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using CoCSharp.Networking.Packets;
+using System;
+
+namespace CoCSharp.Client.API.Events
+{
+    public class AllianceInfoEventArgs : EventArgs
+    {
+        public AllianceInfoEventArgs(AllianceInfoResponsePacket packet)
+        {
+            Packet = packet;
+        }
+
+        public AllianceInfoResponsePacket Packet { get; private set; }
+    }
+}
